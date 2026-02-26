90da90c65c2785b0aaf7e87a190a60f0956f4200
zhangjipeng <zhangjipeng@xiaomi.com>
2026-02-26 10:30:00 +0800

    test(semantics): cover compound assignment to const locals
    Signed-off-by: zhangjipeng <zhangjipeng@xiaomi.com>

--- tests/test_const.py
+++ tests/test_const.py
@@ -32,3 +32,20 @@ int main(){
     comp = Compiler(optimize=False)
     res = comp.compile_file(str(c_path), str(out_path))
     assert res.success
+
+
+def test_const_local_compound_assignment_is_error(tmp_path):
+    code = r'''
+int main(){
+  const int x = 1;
+  x += 1;
+  return 0;
+}
+'''.lstrip()
+    c_path = tmp_path / "t.c"
+    out_path = tmp_path / "t"
+    c_path.write_text(code)
+
+    comp = Compiler(optimize=False)
+    res = comp.compile_file(str(c_path), str(out_path))
+    assert not res.success
